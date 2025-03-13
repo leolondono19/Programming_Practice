00cf28538404bf21f3ffa524fc41f5d751855a26
leolondono19 <leoneelondono@gmail.com>
2025-03-12 21:57:59 -0400

    3 resueltos

---
+++ LeetCodePractice/LeetCodePractice/BinaryString.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace LeetCodePractice
+{
+    class BinaryString
+    {
+        public string AddBinary(string a, string b)
+        {
+            int lengthA = a.Length;
+            int lengthB = b.Length;
+            string longString, shortString;
+            char carry = '0';
+            string result = "";
+
+            if (lengthA >= lengthB)
+            {
+                longString = a;
+                shortString = b;
+            }
+            else
+            {
+                longString = b;
+                shortString = a;
+            }
+
+            int longIdx = longString.Length - 1;
+            int shortIdx = shortString.Length - 1;
+
+            while (longIdx >= 0)
+            {
+                (char result, char carry) test;
+                if (shortIdx >= 0)
+                {
+                    test = AddBinaryDigits(longString[longIdx], shortString[shortIdx], carry);
+                }
+                else
+                {
+                    test = AddBinaryDigits(longString[longIdx], '0', carry);
+                }
+                longIdx--;
+                shortIdx--;
+                carry = test.carry;
+                result = result.Insert(0, test.result.ToString());
+            }
+            if (carry == '1')
+            {
+                result = result.Insert(0, "1");
+            }
+            return result;
+        }
+        public (char result, char carry) AddBinaryDigits(char a, char b, char c)
+        {
+            if (a != b)
+            {
+                if(c == '0')
+                {
+                    return ('1', '0');
+                }
+                else
+                {
+                    return ('0', '1');
+                }
+            }
+            else if (a == '0' && b == '0')
+            {
+                if (c == '0')
+                {
+                    return ('0', '0');
+                }
+                else
+                {
+                    return ('1', '0');
+                }
+            }
+            else if (a == '1' && b == '1')
+            {
+                if(c == '0')
+                {
+                    return ('0', '1');
+                }
+                else
+                {
+                    return ('1', '1');
+                }
+            }
+            throw new InvalidOperationException();
+        }
+    }
+}
+
+/*
+
+0101101010101011001
+      0101010101010
+
+11111
+11111
+
+0 + 0 = 0
+0 + 1 = 1
+1 + 0 = 1
+1 + 1 = 10
+
+0 + 0 + 0
+0 + 1 + 1
+1 + 0 = 
+1 + 1 + 1= 11
+ 
+ */
---
+++ LeetCodePractice/LeetCodePractice/SubstringsInWord.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice
+{
+    class SubstringsInWord
+    {
+        public int NumOfStrings(string[] patterns, string word)
+        {
+            int numberOfStrings = 0;
+            foreach (var item in patterns)
+            {
+                if(word.Contains(item))
+                {
+                    numberOfStrings++;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return numberOfStrings;
+        }
+    }
+}
+
+/*
+ PROBLEMA:
+Tenemos una cadena de strings y una palabra string
+Se debe contar la cantidad de strings de la cadena, que son parte o estan dentro de la palabra
+
+EJEMPLO:    
+Input: patterns = ["a","abc","bc","d"], word = "abc"
+Output: 3
+Explanation:
+- "a" appears as a substring in "abc".
+- "abc" appears as a substring in "abc".
+- "bc" appears as a substring in "abc".
+- "d" does not appear as a substring in "abc".
+3 of the strings in patterns appear as a substring in word.
+
+SOLUCION:
+Primero agarraremos los strings de la cadena en orden
+Verificaremos si el string que se agarro, se jhace presente dentro de la palabra
+si es asi, se suma a un contador, en caso de que no se encuentre en la palabra, se continuara revisando
+hasta acabar con todas las palabras de la cadena.
+ 
+ */
